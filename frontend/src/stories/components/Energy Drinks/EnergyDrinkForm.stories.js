2bb23243a7b205bb8af1db9449230c0ce36959c9
Simon Lai <simonlai@ucsb.edu>
2023-04-27 14:48:33 -0700

    sl - add 3 form parts

---
+++ frontend/src/stories/components/Energy Drinks/EnergyDrinkForm.stories.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import EnergyDrinkForm from "main/components/EnergyDrinks/EnergyDrinkForm"
+import { energydrinkFixtures } from 'fixtures/energydrinkFixtures';
+
+export default {
+    title: 'components/Energy Drinks/EnergyDrinkForm',
+    component: EnergyDrinkForm
+};
+
+const Template = (args) => {
+    return (
+        <EnergyDrinkForm {...args} />
+    )
+};
+
+export const Default = Template.bind({});
+
+Default.args = {
+    submitText: "Create",
+    submitAction: () => { console.log("Submit was clicked"); }
+};
+
+export const Show = Template.bind({});
+
+Show.args = {
+    EnergyDrink: energydrinkFixtures.oneEnergyDrink,
+    submitText: "",
+    submitAction: () => { }
+};

e7b7f585e4b7df8a585547c451d640e86fcc8935
leon_feng <b_feng@ucsb.edu>
2023-04-27 14:26:48 -0700

    initial push WIP

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,6 +10,9 @@ import RestaurantEditPage from "main/pages/Restaurants/RestaurantEditPage";
 import RestaurantIndexPage from "main/pages/Restaurants/RestaurantIndexPage";
 import RestaurantDetailsPage from "main/pages/Restaurants/RestaurantDetailsPage";
 
+import BookCreatePage from "main/pages/Books/BookCreatePage";
+import BookEditPage from "main/pages/Books/BookEditPage";
+
 
 function App() {
 
@@ -30,6 +33,8 @@ function App() {
         <Route exact path="/restaurants/details/:id" element={<RestaurantDetailsPage />} />
         <Route exact path="/restaurants/" element={<RestaurantIndexPage />} />
 
+        <Route exact path="/books/create" element={<BookCreatePage/>}/>
+        <Route exact path="/books/edit/:id" element={<BookEditPage/>}/>
       </Routes>
     </BrowserRouter>
   );
